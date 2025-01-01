b9d4e788e18f9d0741d56ac26d77046da32de168
Matthew Kavanagh <matthewfkavanagh@icloud.com>
2025-01-01 15:57:16 -0500

    Add setup script for proper nvim LSP linking

---
+++ ESP32/RF_Transmitter/setup.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+
+# First, create a new project using :Pioinit
+# Next, delete the old 'compile_commands.json' file.
+# Then, copy this script into the project root, mark as executable, and run!
+
+echo 'CompileFlags:                    
+  Add:
+    [
+      # -mlong-calls,
+      -DSSIZE_MAX,
+      -DLWIP_NO_UNISTD_H=1,
+      -Dssize_t=long,
+      -D_SSIZE_T_DECLARED,
+    ]
+  Remove:
+    [
+      -fno-tree-switch-conversion,
+      -mtext-section-literals,
+      -mlongcalls,
+      -fstrict-volatile-bitfields,
+      -free,
+      -fipa-pta,
+    ]' > .clangd
+
+echo "Checks: '-*, -misc-definitions-in-headers' " > .clangd-tidy
+
+echo 'import os
+Import("env")
+
+# include toolchain paths
+env.Replace(COMPILATIONDB_INCLUDE_TOOLCHAIN=True)
+
+# override compilation DB path
+env.Replace(COMPILATIONDB_PATH="compile_commands.json")' > gen_compile_commands.py
+
+echo 'extra_scripts = pre:gen_compile_commands.py' >> platformio.ini
+
+pio run -t compiledb
+
+exit 0
