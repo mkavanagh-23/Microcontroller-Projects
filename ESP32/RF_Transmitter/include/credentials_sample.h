21340e235453c3b1ba2e75dae002dae5ae5a05a4
Matthew Kavanagh <matthewfkavanagh@icloud.com>
2025-01-01 16:00:53 -0500

    Fix issue with C++ standard

--- ESP32/RF_Transmitter/include/credentials_sample.h
+++ ESP32/RF_Transmitter/include/credentials_sample.h
@@ -1,18 +1,20 @@
 #ifndef CREDENTIALS_H
 #define CREDENTIALS_H
 
+#include <string>
+
 // Substitute your values below and rename file as credentials.h
 
 // Fill-in your WiFi details below
-constexpr char* WIFI_SSID = "";
-constexpr char* WIFI_PASSWORD = "";
+const std::string WIFI_SSID = "";
+const std::string WIFI_PASSWORD = "";
 
 // Fill-in details for RF transmitter below
 namespace Transmitter {
   constexpr short PROTOCOL = 0;
   constexpr short PULSE_LENGTH = 0;
-  constexpr char* SWITCH_ON = ""; // Binary code for turning the switch on
-  constexpr char* SWITCH_OFF = "";  // Binary code for turning the switch off
+  const std::string SWITCH_ON = ""; // Binary code for turning the switch on
+  const std::string SWITCH_OFF = "";  // Binary code for turning the switch off
 }
 
 #endif
